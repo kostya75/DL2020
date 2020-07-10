db1f57f58d8e1e2174a4cedbb57dafeb2c1ad8d1
kostya75 <kmingoulin@gmail.com>
2020-07-10 08:51:07 -0400

    jul 10

--- tensor_Targeting.R
+++ tensor_Targeting.R
@@ -13,12 +13,14 @@ library(keras)
 
 model3 <- keras_model_sequential() %>%
   #layer_flatten(input_shape = c(3,64,64)) %>%
-  layer_dense(units = 50, activation = "relu") %>% 
-  layer_dropout(0.25) %>% 
-  layer_dense(units = 25, activation = "relu") %>% 
-  layer_dropout(0.25) %>% 
-  layer_dense(units = 12, activation = "relu") %>% 
-  layer_dropout(0.25) %>% 
+  layer_dense(units = 32, activation = "relu") %>%
+  layer_dropout(0.2) %>%
+  layer_dense(units = 24, activation = "relu") %>%
+  layer_dropout(0.2) %>%
+  # layer_dense(units = 64, activation = "relu") %>%
+  # layer_dropout(0.2) %>%
+  layer_dense(units = 16, activation = "relu") %>% 
+  layer_dropout(0.2) %>% 
   layer_dense(1, activation = "sigmoid")
 
 #summary(model3)
@@ -34,11 +36,11 @@ model3 %>%
 model3 %>% 
   fit(
     x = as.matrix(df_train_lr[,-1]), y = df_train_lr[,1],
-    epochs = 700,
+    epochs = 4000,
     learning_rate=.0001,
-    validation_split = 0.3,
-    batch_size=32,
-    verbose = 2
+    validation_split = 0.2,
+    batch_size=64,
+    verbose = 1
   )
 
 # predictions <- predict_class(model3, df_test_lr[,-1]))
